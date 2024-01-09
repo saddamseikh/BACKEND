e7a84150b3adbc0af06e263f2eb96c8fc68f2f6e
saddam seikh <saddamseikh2229@gmail.com>
2024-01-09 14:43:04 +0530

    config mulert and cloudinary

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,42 @@
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// export const upload = multer({
+//   storage,
+// });
+
+import multer from "multer";
+const upload = multer({
+  dest: "uploads/",
+  limits: { fileSize: 90 * 1024 * 1024 }, // 50 mb in size max limit
+  storage: multer.diskStorage({
+    destination: "uploads/",
+    filename: (_req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+  //   fileFilter: (_req, file, cb) => {
+  //     let ext = path.extname(file.originalname);
+
+  //     if (
+  //       ext !== ".jpeg" &&
+  //       ext !== ".mp4" &&
+  //       ext !== ".mkv" &&
+  //       ext !== ".png" &&
+  //       ext !== ".mov"
+  //     ) {
+  //       cb(new Error(`Unsupported file type! ${ext}`), false);
+  //       return;
+  //     }
+
+  //     cb(null, true);
+  //   },
+});
+
+export default upload;
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_API,
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been upload successfully
+    console.log("file uploaded successfully :", response.url);
+    // console.log("format", response.format);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return nul;
+  }
+};
+
+export { uploadOnCloudinary };
+
+// cloudinary.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );
